1de5b1f7528d2d6aaaed71f046b1548282171902
dong-gwang <dong.gwang1996@gmail.com>
2022-02-17 20:36:58 +0900

    ✨ [오늘의 할 일] 신동광 SchedulerComponent.css 파일 추가 ✨ [오늘의 할 일] 신동광 SchedulerComponent 프로그래스바, 할일 리스트 추가

--- src/containers/scheduler/schedulerComponent.js
+++ src/containers/scheduler/schedulerComponent.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import './schedulerComponent.css';
+import Checkbox from "@mui/material/Checkbox";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import LinearProgress from "@mui/material/LinearProgress";
+import AddIcon from '@mui/icons-material/Add';
+import CircleIcon from '@mui/icons-material/Circle';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+
 
 const SchedulerComponent = () => (
+
    <div>
-      <h4>SchedulerComponent</h4>
+      <h4>
+         오늘의 할 일 <AddIcon />
+      </h4>
+      <LinearProgress variant="determinate" className='progressBar'/>
+      
+         <FormControlLabel control={<Checkbox defaultChecked />} label="아로마테라피 하기" />
+         <CircleIcon />
+         <br />
+         <FormControlLabel control={<Checkbox defaultChecked />} label="전자기기 사용 줄이기" />
+         <CircleIcon />
+         <br />
+         <FormControlLabel control={<Checkbox defaultChecked />} label="나에 대한 응원의 말 작성" />
+         <CircleIcon />
+         <br />
+         <MoreHorizIcon />
    </div>
 )
 
 export default SchedulerComponent
+
